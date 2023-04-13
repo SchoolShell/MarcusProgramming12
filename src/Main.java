57d878439c9135264a264a03eb27339fd8ff7773
Marcus <invalid>
2023-04-12 21:36:38 -0700

    quadrant

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = Integer.parseInt(scanner.nextLine());
+        int y = Integer.parseInt(scanner.nextLine());
+        if (x == 0 || y == 0) {
+            System.out.println("Quadrant unknown");
+        } else {
+            if (x > 0) {
+                if (y > 0) {
+                    System.out.println("Quadrant 1");
+                } else {
+                    System.out.println("Quadrant 4");
+                }
+            } else {
+                if (y > 0) {
+                    System.out.println("Quadrant 2");
+                } else {
+                    System.out.println("Quadrant 3");
+                }
+            }
+        }
     }
 }
 
